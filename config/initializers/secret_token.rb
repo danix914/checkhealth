4277e1504af83dd630dba0655176d7c14c2ab052
Det <danix914@gmail.com>
2012-04-24 20:34:51 +0800

    1st init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Health::Application.config.secret_token = '836202c400341ae2ad1d03cc2d6e876c72e6f62db6ca163f9fba92744b1a508e71d3121dff4c8701a1b8da4eeea4bbbc2598453c83d2dbd762ca187310692ed2'
